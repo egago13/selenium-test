ec4555b214db18c6a7662ddd9dba59cbee182fdd
egago13 <egakusnadi96@gmail.com>
2022-01-21 09:21:46 +0700

    remote dan menambahkan aksi

--- src/hello_selenium.java
+++ src/hello_selenium.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -5,8 +6,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class hello_selenium {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\egaku\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        WebDriver obj = new ChromeDriver();
-        obj.get("https://www.github.com");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.twitter.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("email_container")).sendKeys("egacorelone@gmail.com");
 
     }
 }
