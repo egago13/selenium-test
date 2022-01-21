4e26f0b92a097bdfa7a37ec285c174aa994ddeda
egago13 <egakusnadi96@gmail.com>
2022-01-21 07:34:40 +0700

    remote to googe.com

---
+++ src/hello_selenium.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class hello_selenium {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\egaku\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver obj = new ChromeDriver();
+        obj.get("https://www.google.com");
+
+    }
+}
